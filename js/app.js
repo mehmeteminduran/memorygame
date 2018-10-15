12111693bdf5cb1d43ca771c533a8d11716aa410
Mehmet Emin DURAN <memin.duran@stm.com.tr>
2018-10-15 11:53:50 +0300

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-/*
- * Create a list that holds all of your cards
- */
+// Variables
 const distinctCardTypes = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 const cardMatchClassName = 'match';
 const showCardClassName = 'show';
 const openCardClassName = 'open';
+const cardClassName = 'card';
 let listOfOpenedCards = [];
+let moveCount = 0;
+let moves = document.querySelector('.moves');
 
-// Find list of cards which are doubled and shuffled.
-let listOfCardTypes = shuffle(doubleArrayElements(distinctCardTypes));
-
-// Find container and 
-let container = document.querySelector('.container');
-
+// Get list of cards which are doubled and shuffled.
+let listOfCardTypes = shuffle(doubleArrayElements(distinctCardTypes)); 
+// Find container element 
+let container = document.querySelector('.container'); 
 // Add unordered list to container
 let unorderedList = document.createElement('ul');
-unorderedList.className = 'deck';
-
+unorderedList.className = 'deck'; 
 // add each card's HTML to unordered list
 for (const cardType of listOfCardTypes) {
     let card = createCardElement(cardType);
@@ -34,86 +32,6 @@ function createCardElement(cardClassName) {
     cardText.classList.add(cardClassName);
     element.appendChild(cardText);
     return element;
-}
-
-function onCardClicked(event) {
-    if (event.target.classList.contains(openCardClassName)) {
-        return;
-    } else {
-        debugger;
-        if (listOfOpenedCards.length < 2) {
-            displayCardSymbol(event.target);
-        }
-        
-        setTimeout(function () {
-            addToOpenedCardList(event.target.firstElementChild.className);
-        }, 300);
-        
-
-    }
-}
-
-function displayCardSymbol(target) {
-    debugger;
-    target.classList.add(showCardClassName);
-    target.classList.add(openCardClassName);
-}
-
-function addToOpenedCardList(childElementClassName) {
-    listOfOpenedCards.push(childElementClassName);
-    if (listOfOpenedCards != null && listOfOpenedCards.length == 2) {
-        let openedCards = document.getElementsByClassName(openCardClassName);
-
-        let openedCardSpanElements = [];
-        for (const openedCard of openedCards) {
-            openedCardSpanElements.push(openedCard.firstElementChild.className);
-        }
-
-        if (allTheSame(openedCardSpanElements)) {
-            lockOpenedCards(openedCards);
-        } else {
-            for (const openedCardSpanElement of openedCardSpanElements) {
-                listOfOpenedCards.pop(openedCardSpanElement);
-            }
-            closeOpenedCards(openedCards);
-        }
-    }
-}
-
-function allTheSame(array) {
-    var first = array[0];
-    return array.every(function (element) {
-        return element === first;
-    });
-}
-
-
-function lockOpenedCards(openedCards) {
-    debugger;
-    for (let i = 0; openedCards.length; i++) {
-        openedCards[0].classList.remove(showCardClassName);
-        openedCards[0].classList.remove(openCardClassName);
-        openedCards[0].classList.add(cardMatchClassName);
-    }
-
-}
-
-function closeOpenedCards(openedCards) {
-
-    // openedCards.forEach(function(ele, idx) {
-    //     ele.classList.remove(showCardClassName);
-    //     ele.classList.remove(openCardClassName);  
-    //  })
-
-    debugger;
-    for (let i = 0; openedCards.length; i++) {
-        openedCards[0].classList.remove(showCardClassName);
-        openedCards[0].classList.remove(openCardClassName);
-    }
-    // for (let openedCard of openedCards) {
-    //     openedCard.classList.remove(showCardClassName);
-    //     openedCard.classList.remove(openCardClassName);  
-    // }
 }
 
 // This function from doubles up each element in a array
@@ -138,15 +56,87 @@ function shuffle(array) {
     return array;
 }
 
+// Card click Event
+function onCardClicked(event) { 
+    if (isCardOpenOrMatched(event.target)) {
+        return;
+    } else { 
+        increaseMoveCount();
+        displayCardSymbol(event.target);
+        setTimeout(function () {
+            addToOpenedCardList(event.target.firstElementChild.className);
+        }, 300);
+    }
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Check whether element or parent element has open or match class .İf so, do nothing.
+function isCardOpenOrMatched(target) { 
+    return target.classList.contains(openCardClassName) || target.classList.contains(cardMatchClassName)
+        || target.parentElement.classList.contains(openCardClassName) || target.parentElement.classList.contains(cardMatchClassName);
+}
 
+// open and show card
+function displayCardSymbol(target) { 
+    target.classList.add(showCardClassName);
+    target.classList.add(openCardClassName);
+}
+
+// İf opened cards are same lock the cards,otherwise close cards.
+function addToOpenedCardList(childElementClassName) {
+    listOfOpenedCards.push(childElementClassName);
+    if (listOfOpenedCards != null && listOfOpenedCards.length == 2) {
+        let openedCards = document.getElementsByClassName(openCardClassName); 
+        if (allTheSame(listOfOpenedCards)) {
+            lockOpenedCards(openedCards);
+        } else {
+            closeOpenedCards(openedCards);
+        }
+        listOfOpenedCards = [];
+    }
+}
+
+// Check All elements in a array are all same
+function allTheSame(array) {
+    var first = array[0];
+    return array.every(function (element) {
+        return element === first;
+    });
+}
+
+// Set opened cards as matched.
+function lockOpenedCards(openedCards) {
+    debugger;
+    for (let i = 0; openedCards.length; i++) {
+        openedCards[0].classList.add(cardMatchClassName);
+        openedCards[0].classList.remove(showCardClassName);
+        openedCards[0].classList.remove(openCardClassName);
+    }
+
+    if (isAllCardsMatched()) {
+        alert('All cards are matched');
+    }
+}
+
+// Check whether all cards are matched
+function isAllCardsMatched() {
+    const cardClassCount = document.getElementsByClassName(cardClassName).length;
+    const matchedCardClassCount = document.getElementsByClassName(cardMatchClassName).length;
+    return cardClassCount == matchedCardClassCount;
+}
+
+// Close opened cards if not same
+function closeOpenedCards(openedCards) {
+    debugger;
+    for (let i = 0; openedCards.length; i++) {
+        openedCards[0].classList.remove(showCardClassName);
+        openedCards[0].classList.remove(openCardClassName);
+    }
+}
+
+// Increase move count
+function increaseMoveCount() {
+    moveCount++;
+    moves.textContent = moveCount;
+}
+
+// TODO : Restart and star count
